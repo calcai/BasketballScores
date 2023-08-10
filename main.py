b884438058178c572d3a176eec049b73d020917c
Calvin <cyc44@case.edu>
2023-08-10 15:39:19 -0700

    simple flask app, fixed end of month bug

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    scores = None
+    if request.method == 'POST':
+        date = request.form['date']
+        month, day, year = map(int, date.split('/'))
+        scores = retrieve_scores(month, day, year)
+    return render_template('index.html', scores=scores)
 
 def retrieve_scores(month, day, year):
 
@@ -7,6 +19,12 @@ def retrieve_scores(month, day, year):
     month_names = {
         1: "january", 2: "february", 3: "march", 4: "april", 5: "may", 6: "june",
         7: "july", 8: "august", 9: "september", 10: "october", 11: "november", 12: "december"
+    }
+
+    #maps numerical month to shorthand 
+    month_shorthand = {
+        1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun",
+        7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"
     }
 
     #adjusts year for url because the year in url corresponds to the second year in a season
@@ -30,7 +48,7 @@ def retrieve_scores(month, day, year):
     
     #extract dates from table and match row to date
     dates = body[0].find_all("th")
-    rowvals = [rownum for rownum, date in enumerate(dates, start=1) if f"{day}, {year}" in date.get_text()]
+    rowvals = [rownum for rownum, date in enumerate(dates, start=1) if f"{month_shorthand[month]} {day}, {year}" in date.get_text()]
 
     #checks if no games are played on this day
     if not rowvals:
@@ -49,20 +67,10 @@ def retrieve_scores(month, day, year):
             row_cells = rows[rownum].find_all("td")
             visitor, visitor_score, home, home_score = [cell.get_text() for cell in row_cells[0:4]]
         else:
-            row_cells = rows[rownum + 1].find_all("td")
+            row_cells = rows[rownum].find_all("td")
             visitor, visitor_score, home, home_score = [cell.get_text() for cell in row_cells[1:5]]
         scores.append(f"{visitor}: {visitor_score} | {home}: {home_score}")
 
     return scores
-
-def main():
-    date = input("Enter a date (MM/DD/YYYY): ")
-    month, day, year = map(int, date.split('/'))
-
-    scores = retrieve_scores(month, day, year)
-
-    for score in scores:
-        print(score)
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
